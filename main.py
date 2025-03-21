05aeb6691c3bdcf1cbfc5f356f235a7640072796
ankan <hamburgerzeta@gmail.com>
2025-03-21 19:33:24 +0000

    persist

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from lib import datamanipulation, dataReader, utils, datetime
 from pyspark.sql.functions import *  # type: ignore
-
+from pyspark import StorageLevel
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please specify the environment")
@@ -19,16 +19,17 @@ if __name__ == '__main__':
     df = dataReader.read_data(spark, job_run_env)
     df_filtered = datamanipulation.lost_orders(df)    
     df_filtered.show(truncate = False)
-    df_filtered.write.mode("overwrite").parquet(f"hdfs://localhost:9000/user/vboxuser/retail_db/lost_revenue/{dt}/lost_revenue_{ts}.parquet")
+    df_filtered.write.mode("overwrite").option("parquet.block.size", 268435456).parquet(f"hdfs://localhost:9000/user/vboxuser/retail_db/lost_revenue/{dt}/lost_revenue_{ts}.parquet")
     print("Finshed writing lost to HDFS")
-    df_filtered = df_filtered.cache()
+    df_filtered = df_filtered.persist(StorageLevel.MEMORY_AND_DISK)
     df_filtered.count()
     grouping = datamanipulation.grouped_df(df_filtered)
 
-    grouping = grouping.cache()
+    grouping = grouping.persist(StorageLevel.MEMORY_AND_DISK)
     grouping.count()
+
     print("Writing to HDFS")
-    grouping.write.mode("overwrite").partitionBy("StockCode").parquet(f"hdfs://localhost:9000/user/vboxuser/retail_db/lost_revenue/{dt}/lost_revenue_{ts}.parquet")
+    grouping.write.mode("overwrite").option("parquet.block.size", 268435456).partitionBy("StockCode").parquet(f"hdfs://localhost:9000/user/vboxuser/retail_db/lost_revenue/aggregated/{dt}/lost_revenue_{ts}.parquet")
     print("Finshed writing aggregations to HDFS")
     print("Stopping Spark Session")
     spark.stop()
